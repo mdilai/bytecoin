a1ead64616d32fe985aa6427b3ca9c6611699ef7
Maksym Dilai <admin@dproject.org.ua>
2017-03-20 23:49:42 +0200

    Revert "Updated miniupnpc"
    This reverts commit 98137b3af4c2319be34886c65c3a5d4ec4cfcb3a.

--- external/miniupnpc/upnpreplyparse.c
+++ external/miniupnpc/upnpreplyparse.c
@@ -1,7 +1,7 @@
-/* $Id: upnpreplyparse.c,v 1.19 2015/07/15 10:29:11 nanard Exp $ */
+/* $Id: upnpreplyparse.c,v 1.15 2013/06/06 21:36:40 nanard Exp $ */
 /* MiniUPnP project
  * http://miniupnp.free.fr/ or http://miniupnp.tuxfamily.org/
- * (c) 2006-2015 Thomas Bernard
+ * (c) 2006-2013 Thomas Bernard
  * This software is subject to the conditions detailed
  * in the LICENCE file provided within the distribution */
 
@@ -40,15 +40,6 @@ NameValueParserEndElt(void * d, const char * name, int l)
 		/* standard case. Limited to n chars strings */
 		l = data->cdatalen;
 	    nv = malloc(sizeof(struct NameValue));
-		if(nv == NULL)
-		{
-			/* malloc error */
-#ifdef DEBUG
-			fprintf(stderr, "%s: error allocating memory",
-			        "NameValueParserEndElt");
-#endif /* DEBUG */
-			return;
-		}
 	    if(l>=(int)sizeof(nv->value))
 	        l = sizeof(nv->value) - 1;
 	    strncpy(nv->name, data->curelt, 64);
@@ -62,8 +53,7 @@ NameValueParserEndElt(void * d, const char * name, int l)
 		{
 			nv->value[0] = '\0';
 		}
-		nv->l_next = data->l_head;	/* insert in list */
-		data->l_head = nv;
+	    LIST_INSERT_HEAD( &(data->head), nv, entries);
 	}
 	data->cdata = NULL;
 	data->cdatalen = 0;
@@ -81,10 +71,6 @@ NameValueParserGetData(void * d, const char * datas, int l)
 		if(!data->portListing)
 		{
 			/* malloc error */
-#ifdef DEBUG
-			fprintf(stderr, "%s: error allocating memory",
-			        "NameValueParserGetData");
-#endif /* DEBUG */
 			return;
 		}
 		memcpy(data->portListing, datas, l);
@@ -103,19 +89,19 @@ void
 ParseNameValue(const char * buffer, int bufsize,
                struct NameValueParserData * data)
 {
-	struct xmlparser parser;
-	data->l_head = NULL;
+    struct xmlparser parser;
+    LIST_INIT(&(data->head));
 	data->portListing = NULL;
 	data->portListingLength = 0;
-	/* init xmlparser object */
-	parser.xmlstart = buffer;
-	parser.xmlsize = bufsize;
-	parser.data = data;
-	parser.starteltfunc = NameValueParserStartElt;
-	parser.endeltfunc = NameValueParserEndElt;
-	parser.datafunc = NameValueParserGetData;
+    /* init xmlparser object */
+    parser.xmlstart = buffer;
+    parser.xmlsize = bufsize;
+    parser.data = data;
+    parser.starteltfunc = NameValueParserStartElt;
+    parser.endeltfunc = NameValueParserEndElt;
+    parser.datafunc = NameValueParserGetData;
 	parser.attfunc = 0;
-	parsexml(&parser);
+    parsexml(&parser);
 }
 
 void
@@ -128,9 +114,9 @@ ClearNameValueList(struct NameValueParserData * pdata)
 		pdata->portListing = NULL;
 		pdata->portListingLength = 0;
 	}
-    while((nv = pdata->l_head) != NULL)
+    while((nv = pdata->head.lh_first) != NULL)
     {
-		pdata->l_head = nv->l_next;
+        LIST_REMOVE(nv, entries);
         free(nv);
     }
 }
@@ -141,9 +127,9 @@ GetValueFromNameValueList(struct NameValueParserData * pdata,
 {
     struct NameValue * nv;
     char * p = NULL;
-    for(nv = pdata->l_head;
+    for(nv = pdata->head.lh_first;
         (nv != NULL) && (p == NULL);
-        nv = nv->l_next)
+        nv = nv->entries.le_next)
     {
         if(strcmp(nv->name, Name) == 0)
             p = nv->value;
@@ -185,13 +171,13 @@ DisplayNameValueList(char * buffer, int bufsize)
     struct NameValueParserData pdata;
     struct NameValue * nv;
     ParseNameValue(buffer, bufsize, &pdata);
-    for(nv = pdata.l_head;
+    for(nv = pdata.head.lh_first;
         nv != NULL;
-        nv = nv->l_next)
+        nv = nv->entries.le_next)
     {
         printf("%s = %s\n", nv->name, nv->value);
     }
     ClearNameValueList(&pdata);
 }
-#endif /* DEBUG */
+#endif
 
